2893af533c210549ca8b0d85e0ea3d00d3fb65d9
Shivam <SINGHSHIVAMKR@GMAIL.COM>
2020-06-18 23:07:42 +0530

    fixed random function

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const previousNums = [];
 export const getRandomFact = () => {
   const randNum = getRandomItem();
 
-  if (facts.includes(randNum)) {
+  if (previousNums.includes(randNum)) {
     //if the number is already there call the function again
     getRandomFact();
   } else {
