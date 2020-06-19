86dd831d209647bb93aa7e79d0c76870a82ebf93
Shivam <SINGHSHIVAMKR@GMAIL.COM>
2020-06-19 11:28:56 +0530

    improved fixed

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,14 @@
 import json from "@rollup/plugin-json";
 
 const dist = "dist";
+
 export default {
   input: ["src/index.js"],
-  output: [
-    {
-      file: `${dist}/bundle.cjs.js`,
-      format: "cjs",
-    },
-    {
-      file: `${dist}/bundle.esm.js`,
-      format: "esm",
-    },
-    {
-      name: "FactsQuiz",
-      file: `${dist}/bundle.umd.js`,
-      format: "umd",
-    },
-  ],
+  output: {
+    name: "fq",
+    file: `${dist}/factsquiz.js`,
+    format: "umd",
+  },
+
   plugins: [json()],
 };
