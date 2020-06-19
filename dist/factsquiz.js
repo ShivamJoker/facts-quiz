a01c5717ed6d4b0b3e3c85f6b36ce47aa3e735e4
Shivam <SINGHSHIVAMKR@GMAIL.COM>
2020-06-19 11:29:19 +0530

    build done

--- dist/factsquiz.js
+++ dist/factsquiz.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = global || self, factory(global.FactsQuiz = {}));
+  (global = global || self, factory(global.fq = {}));
 }(this, (function (exports) { 'use strict';
 
   var facts = [
